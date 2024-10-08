7911742c77524325cce7a060794db5a06e203db0
Aniket Raj <aniketraj.cse@gmail.com>
2024-10-08 17:42:25 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLo9CQepw-Sz3004y_lzzp8aOqgYeU5Eo",
+  authDomain: "crypto-xpress.firebaseapp.com",
+  projectId: "crypto-xpress",
+  storageBucket: "crypto-xpress.appspot.com",
+  messagingSenderId: "619884856236",
+  appId: "1:619884856236:web:d772fa151f60fa810bb009"
+};
+
+export default firebaseConfig;
